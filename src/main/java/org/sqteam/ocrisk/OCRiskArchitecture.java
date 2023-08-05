ae180995ddb4632a890cbe242d7721493cd8ca81
artem.kabakov <artem.kabakov@estcube.eu>
2023-08-05 05:43:49 +0300

    add basic java api

--- src/main/java/org/sqteam/ocrisk/OCRiskArchitecture.java
+++ src/main/java/org/sqteam/ocrisk/OCRiskArchitecture.java
@@ -1,12 +1,16 @@
 package org.sqteam.ocrisk;
 
-import li.cil.oc.api.machine.Architecture;
-import li.cil.oc.api.machine.ExecutionResult;
-import li.cil.oc.api.machine.Machine;
+import li.cil.oc.api.machine.*;
+import li.cil.oc.api.network.Component;
+import li.cil.oc.api.network.Node;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 @Architecture.Name("OCRisk")
 public class OCRiskArchitecture implements Architecture {
@@ -26,6 +30,7 @@ public class OCRiskArchitecture implements Architecture {
 
     @Override
     public boolean recomputeMemory(Iterable<ItemStack> iterable) {
+        logger.info("recompute: {}", new ArrayList<>((Collection) iterable));
         return true;
     }
 
@@ -47,7 +52,35 @@ public class OCRiskArchitecture implements Architecture {
 
     @Override
     public ExecutionResult runThreaded(boolean b) {
-        return new ExecutionResult.Error("Not implemented");
+        Signal signal = machine.popSignal();
+        //
+        if (signal != null) {
+            Iterable<Node> nodes = machine.node().reachableNodes();
+            logger.info("{} - {}", signal.name(), signal.args());
+            //machine.node().sendToAddress("b793c238-6e5d-4a64-b438-ca3a3cb4b061", "print", );
+            final Node node = machine.node().network().node("495f2bc7-7c83-403b-88f5-3ed44e90e454");
+            if (node instanceof Component) {
+                final Component component = (Component) node;
+                if (component.canBeSeenFrom(machine.node())) {
+                    Map<String, Callback> functions =  machine.methods(node.host());
+                    logger.info("components: {}", functions.get("bind"));
+                }
+            }
+            try {
+                if(signal.name().equals("drop") || signal.name().equals("drag") ) {
+                    logger.info("bind => {}", machine.invoke("495f2bc7-7c83-403b-88f5-3ed44e90e454", "bind", new Object[]{"67459401-8235-46e3-bc56-f124dd26a6ed"}));
+                    logger.info("setForeground => {}", machine.invoke("495f2bc7-7c83-403b-88f5-3ed44e90e454", "setBackground", new Object[]{0x00AA00, false}));
+                    logger.info("fill => {}", machine.invoke("495f2bc7-7c83-403b-88f5-3ed44e90e454", "fill", new Object[]{signal.args()[1], signal.args()[2], 1, 1, " "}));
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            //logger.info("status:", );
+            logger.info("nodes: {}", machine.node().reachableNodes());
+        }
+        return new ExecutionResult.Sleep(0);
     }
 
     @Override
